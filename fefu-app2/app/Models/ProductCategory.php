a5172ba125f9bcd5f3acd5fe2f6855993b6629ce
Skurik-SA <56246154+Skurik-SA@users.noreply.github.com>
2022-06-19 16:51:49 +1000

    Lesson 8: Add products and attributes (web)

--- fefu-app2/app/Models/ProductCategory.php
+++ fefu-app2/app/Models/ProductCategory.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
-use Carbon\Carbon;
-use Cviebrock\EloquentSluggable\Sluggable;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Exception;
 
 /**
  * App\Models\ProductCategory
@@ -18,23 +19,31 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
  * @property string $name
  * @property string $slug
  * @property int|null $parent_id
+ * @property-read Collection|ProductCategory[] $children
+ * @property-read int|null $children_count
+ * @property-read Collection|\App\Models\Product[] $products
+ * @property-read int|null $products_count
  * @method static \Database\Factories\ProductCategoryFactory factory(...$parameters)
- * @method static \Illuminate\Database\Eloquent\Builder|ProductCategory newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|ProductCategory newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|ProductCategory query()
- * @method static \Illuminate\Database\Eloquent\Builder|ProductCategory whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|ProductCategory whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|ProductCategory whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|ProductCategory whereParentId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|ProductCategory whereSlug($value)
- * @method static \Illuminate\Database\Eloquent\Builder|ProductCategory whereUpdatedAt($value)
+ * @method static Builder|ProductCategory findSimilarSlugs(string $attribute, array $config, string $slug)
+ * @method static Builder|ProductCategory newModelQuery()
+ * @method static Builder|ProductCategory newQuery()
+ * @method static Builder|ProductCategory query()
+ * @method static Builder|ProductCategory whereCreatedAt($value)
+ * @method static Builder|ProductCategory whereId($value)
+ * @method static Builder|ProductCategory whereName($value)
+ * @method static Builder|ProductCategory whereParentId($value)
+ * @method static Builder|ProductCategory whereSlug($value)
+ * @method static Builder|ProductCategory whereUpdatedAt($value)
+ * @method static Builder|ProductCategory withUniqueSlugConstraints(\Illuminate\Database\Eloquent\Model $model, string $attribute, array $config, string $slug)
  * @mixin \Eloquent
  */
 class ProductCategory extends Model
 {
     use HasFactory, Sluggable;
 
-    protected array $dates = ['published_at'];
+    protected $fillable = [
+        'name', 'slug', 'parent_id'
+    ];
 
     public function sluggable(): array
     {
@@ -45,8 +54,35 @@ class ProductCategory extends Model
         ];
     }
 
-    public function children(): HasMany {
+    public function children(): HasMany
+    {
         return $this->hasMany(self::class, 'parent_id');
     }
 
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    public static function getTreeProductsBuilder(Collection $categories): Builder
+    {
+        $categoryIds = [];
+
+        if ($categories->isEmpty()){
+            throw new Exception('no categories');
+        }
+
+        $collectCategoryIds = function (ProductCategory $category) use (&$categoryIds, &$collectCategoryIds) {
+            $categoryIds[] = $category->id;
+            foreach ($category->children as $childCategory) {
+                $collectCategoryIds($childCategory);
+            }
+        };
+
+        foreach ($categories as $category){
+            $collectCategoryIds($category);
+        }
+
+        return Product::query()->whereIn('product_category_id', $categoryIds);
+    }
 }
