6ee29b63518f3f126523c78d79947eb476163733
Skurik-SA <56246154+Skurik-SA@users.noreply.github.com>
2022-06-19 19:07:09 +1000

    Lesson 8: Add products and attributes (web & api)

---
+++ fefu-app2/app/OpenApi/Responses/ErrorCatalogResponse.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\OpenApi\Responses;
+
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Response;
+use Vyuldashev\LaravelOpenApi\Factories\ResponseFactory;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\MediaType;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Schema;
+
+class ErrorCatalogResponse extends ResponseFactory
+{
+    /**
+     * @return Response
+     */
+    public function build(): Response
+    {
+        $response = Schema::object()->properties(
+            Schema::string('message')->example('Error'),
+            Schema::string('errors')->example('error category'),
+        );
+
+        return Response::create('Errorcategory')
+            ->description('Category error')
+            ->content(
+                MediaType::json()->schema($response)
+            );
+    }
+}
